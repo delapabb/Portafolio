0f5f034030297186f4c5b26e5c507b831c36fb4e
Pablo De La Rosa <pablodelarosa1984@gmail.com>
2017-04-05 12:41:53 -0500

    Set up front page post cards templates

--- front-page.php
+++ front-page.php
@@ -15,32 +15,52 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
+		$the_query = new WP_Query( 
+			array(
+				'category_name' => 'portfolio'
+			)
+		);
+
+		if ( $the_query->have_posts() ) : ?>
 
 			<header>
 				<h1 class="page-title screen-reader-text show-for-sr"><?php single_post_title(); ?></h1>
 			</header>
 
-			<?php
+			<div class="card-grid">
+				<div class="row">
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					<?php
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+					/* Start the Loop */
+					while ( $the_query->have_posts() ) : $the_query->the_post();
 
-			endwhile;
+						?><div class="small-12 medium-6 large-4 column"><?php
 
-			the_posts_navigation();
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', 'card' );
+
+						?></div><?php
+
+					endwhile;
+
+					the_posts_navigation();
+
+					wp_reset_postdata();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+					?><div class="small-12 column"><?php
+						get_template_part( 'template-parts/content', 'none' );
+					?></div><?php
 
+				?></div><!-- .row -->
+			</div><!-- .card-grid -->
+		<?php
 		endif; ?>
 
 		</main><!-- #main -->
