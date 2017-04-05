71ec92eb6ca42b652f2df7bc6666e5822223352c
Pablo De La Rosa <pablodelarosa1984@gmail.com>
2017-04-05 18:46:49 -0500

    Register case studies post type

--- functions.php
+++ functions.php
@@ -71,6 +71,79 @@ function portafolio_setup() {
 endif;
 add_action( 'after_setup_theme', 'portafolio_setup' );
 
+if ( ! function_exists('case_study_post_type') ) {
+/*
+ * Register Case Study custom post type
+ *
+*/
+
+function case_study_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Case Studies', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Case Study', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Caste Studies', 'text_domain' ),
+		'name_admin_bar'        => __( 'Case Study', 'text_domain' ),
+		'archives'              => __( 'Case Study Archives', 'text_domain' ),
+		'attributes'            => __( 'Case Study Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Case Study:', 'text_domain' ),
+		'all_items'             => __( 'All Case Studies', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Case Study', 'text_domain' ),
+		'add_new'               => __( 'New Case Study', 'text_domain' ),
+		'new_item'              => __( 'New Case Study', 'text_domain' ),
+		'edit_item'             => __( 'Edit Caste Study', 'text_domain' ),
+		'update_item'           => __( 'Update Case Study', 'text_domain' ),
+		'view_item'             => __( 'View Case Study', 'text_domain' ),
+		'view_items'            => __( 'View Case Studies', 'text_domain' ),
+		'search_items'          => __( 'Search Case Study', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into case study', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this case study', 'text_domain' ),
+		'items_list'            => __( 'Case studies', 'text_domain' ),
+		'items_list_navigation' => __( 'Case studies list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter case studies', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                  => 'portfolio',
+		'with_front'            => false,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => __( 'Case Study', 'text_domain' ),
+		'description'           => __( 'A portfolio case study', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-welcome-widgets-menus',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => false,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+		'rest_controller_class' => 'WP_REST_Case_Studies_Controller',
+	);
+	register_post_type( 'case_study', $args );
+
+}
+add_action( 'init', 'case_study_post_type', 0 );
+
+}
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
