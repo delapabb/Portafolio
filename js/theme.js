86c459b1c9abc43b8ab48e068a2e3bd25b01b42e
Pablo De La Rosa <pablodelarosa1984@gmail.com>
2017-04-07 12:55:02 -0500

    Button styles for input elments work-around for pseudo-selectors

--- js/theme.js
+++ js/theme.js
@@ -7,4 +7,7 @@ $(document).foundation();
   		$( ".toggle-bio" ).toggleClass( "bio-open" ).toggleClass( "bio-closed");
 	});
 
+	$("input[type=submit]").before( $('<span class="button round clearfix">') );
+	$("input[type=submit]").before( $('</span>') );
+
 } )( jQuery );
