99ba71f54b45d0da2c0fc05811b12db2b726b673
Pablo De La Rosa <pablodelarosa1984@gmail.com>
2017-04-10 00:25:37 -0500

    Project cards are hoverable and clickable anywhere

--- template-parts/content-card.php
+++ template-parts/content-card.php
@@ -9,43 +9,47 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'card' ); ?> data-equalizer-watch>
-	<header class="entry-header">
+<a href="<?php echo get_permalink(); ?>">
 
-		<?php the_title( '<h2 class="screen-reader-text">', '</h2>' ); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'card' ); ?> data-equalizer-watch>
+		<header class="entry-header">
 
-	</header>
+			<?php the_title( '<h2 class="screen-reader-text">', '</h2>' ); ?>
 
-	<?php
-		if ( has_post_thumbnail() ) :
-			echo get_the_post_thumbnail( $post_id, 'full' ); else : ?>
-			<img src="http://placehold.it/1200x900?text=Select+A+Featured+Image">
-		<?php
-		endif; ?>
-
-	<div class="project-details">
+		</header>
 
 		<?php
+			if ( has_post_thumbnail() ) :
+				echo get_the_post_thumbnail( $post_id, 'full' ); else : ?>
+				<img src="http://placehold.it/1200x900?text=Select+A+Featured+Image">
+			<?php
+			endif; ?>
 
-		the_title( '<p class="title">', '</p>' );
+		<div class="project-details">
 
-		$client = get_post_meta( get_the_ID(), 'client', true );
-		$tools = get_post_meta( get_the_ID(), 'tools', true );
+			<?php
 
-		if( !empty( $client ) ) : ?>
-			<p class="label">Client</p>
-			<p><?php echo $client ?></p>
-		<?php
-		endif;
+			the_title( '<p class="title">', '</p>' );
 
-		if( !empty( $tools ) ) : ?>
-			<p class="label">What I Executed</p>
-			<p class="tools"><?php echo $tools ?></p>
-		<?php
-		endif; ?>
+			$client = get_post_meta( get_the_ID(), 'client', true );
+			$tools = get_post_meta( get_the_ID(), 'tools', true );
 
-	</div>
+			if( !empty( $client ) ) : ?>
+				<p class="label">Client</p>
+				<p><?php echo $client ?></p>
+			<?php
+			endif;
 
-	<a class="button" href="<?php echo get_permalink(); ?>">See This Project</a>
+			if( !empty( $tools ) ) : ?>
+				<p class="label">What I Executed</p>
+				<p class="tools"><?php echo $tools ?></p>
+			<?php
+			endif; ?>
 
-</article><!-- #post-## -->
+		</div>
+
+		<span class="button">See This Project</span>
+
+	</article><!-- #post-## -->
+
+</a>
