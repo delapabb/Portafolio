c93b0719caca01fae340fafa1386c6f89dcbd37b
Pablo De La Rosa <pablodelarosa1984@gmail.com>
2017-04-10 00:09:49 -0500

    Front page project cards show client and contribution

--- template-parts/content-card.php
+++ template-parts/content-card.php
@@ -11,16 +11,41 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'card' ); ?> data-equalizer-watch>
 	<header class="entry-header">
+
+		<?php the_title( '<h2 class="screen-reader-text">', '</h2>' ); ?>
+
+	</header>
+
+	<?php
+		if ( has_post_thumbnail() ) :
+			echo get_the_post_thumbnail( $post_id, 'full' ); else : ?>
+			<img src="http://placehold.it/1200x900?text=Select+A+Featured+Image">
+		<?php
+		endif; ?>
+
+	<div class="project-details">
+
 		<?php
 
-		if ( has_post_thumbnail() ) :
-			echo get_the_post_thumbnail( $post_id, 'full' );
-		else :
-			?><img src="http://placehold.it/1200x900?text=Select+A+Featured+Image"><?php
+		the_title( '<p class="title">', '</p>' );
+
+		$client = get_post_meta( get_the_ID(), 'client', true );
+		$tools = get_post_meta( get_the_ID(), 'tools', true );
+
+		if( !empty( $client ) ) : ?>
+			<p class="label">Client</p>
+			<p><?php echo $client ?></p>
+		<?php
 		endif;
 
-		the_title( '<h2>', '</h2>' );
-		?>
-	</header><!-- .entry-header -->
+		if( !empty( $tools ) ) : ?>
+			<p class="label">What I Executed</p>
+			<p class="tools"><?php echo $tools ?></p>
+		<?php
+		endif; ?>
+
+	</div>
+
 	<a class="button" href="<?php echo get_permalink(); ?>">See This Project</a>
+
 </article><!-- #post-## -->
